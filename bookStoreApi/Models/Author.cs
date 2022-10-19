ee9fc7c04f8c4a7a0436eef6f37069b4b1af07b5
lucero <moreta.concepcion@gmail.com>
2022-10-19 15:46:00 -0400

    add intial commit

---
+++ bookStoreApi/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bookStoreApi.Models
+{
+    public class Author
+    {
+        [Key]
+        public int IdAuthor { get; set; }
+        public string ?Author_Name { get; set; }
+        public string ?Biography { get; set; }
+      
+      
+     
+    }
+}
---
+++ bookStoreApi/Models/Store.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace books_store.Models
+{
+    public class Store
+    {
+        [Key]
+        public int idStore { get; set; }
+        public string store { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+
+
+
+    }
+}
