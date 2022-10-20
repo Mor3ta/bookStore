bafe79082e4db7e830a3408d277a1248e6595668
lucero <moreta.concepcion@gmail.com>
2022-10-20 16:40:47 -0400

    add front-end

---
+++ Front-end/BookList/BookList/Controllers/BooksController.cs
@@ -0,0 +1,28 @@
+﻿using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookList.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Books()
+        {
+            return View();
+        }
+        [Route("Books/Book/{id}")]
+        public IActionResult Book(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
