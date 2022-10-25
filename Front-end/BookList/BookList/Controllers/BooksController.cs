9700530540338fca12e7473e9a0431091b21c209
lucero <moreta.concepcion@gmail.com>
2022-10-25 16:10:59 -0400

    Working on front-end

--- Front-end/BookList/BookList/Controllers/BooksController.cs
+++ Front-end/BookList/BookList/Controllers/BooksController.cs
@@ -24,10 +24,20 @@ namespace BookList.Controllers
             var cliente = new HttpClient();
             cliente.BaseAddress = new Uri("https://localhost:7264");
             var response = await cliente.GetAsync("/api/books");
+            var author_response = await cliente.GetAsync("/api/Author");
+            var genre_response = await cliente.GetAsync("/api/Genre");
+
+
             if (response.IsSuccessStatusCode)
             {
+                
                 var json_response = await response.Content.ReadAsStringAsync();
+                var json_author_response = await author_response.Content.ReadAsStringAsync();
+                var json_genre_response = await genre_response.Content.ReadAsStringAsync();
                 ViewBag.book_list = JsonSerializer.Deserialize<Books[]>(json_response);
+                ViewBag.author = JsonSerializer.Deserialize<Author[]>(json_author_response);
+                ViewBag.genre = JsonSerializer.Deserialize<Genre[]>(json_genre_response);
+
                 return View();
             }
             return View();
