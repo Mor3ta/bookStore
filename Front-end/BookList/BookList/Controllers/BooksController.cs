9188969ac696d2f100f873b640462d5b21938dbb
mor3ta <moreta.concepcion@gmail.com>
2022-10-24 23:56:11 -0400

    add getbook

--- Front-end/BookList/BookList/Controllers/BooksController.cs
+++ Front-end/BookList/BookList/Controllers/BooksController.cs
@@ -1,6 +1,10 @@
-﻿using BookList.Models;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+
+using BookList.Models;
+using static BookList.Models.Books;
+using System.Text.Json;
 
 namespace BookList.Controllers
 {
@@ -8,20 +12,43 @@ namespace BookList.Controllers
     {
         private readonly ILogger<BooksController> _logger;
 
+   
         public BooksController(ILogger<BooksController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Books()
+    
+        public async Task<IActionResult>Books()
         {
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("https://localhost:7264");
+            var response = await cliente.GetAsync("/api/books");
+            if (response.IsSuccessStatusCode)
+            {
+                var json_response = await response.Content.ReadAsStringAsync();
+                ViewBag.book_list = JsonSerializer.Deserialize<Books[]>(json_response);
+                return View();
+            }
             return View();
         }
-        [Route("Books/Book/{id}")]
-        public IActionResult Book(int id)
+
+       
+        public async Task<IActionResult> newBook()
         {
+            var cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("https://localhost:7264");
+
+            //var book = new Books()
+
+
+            // var response = await cliente.PostAsync("/api/books");
+
             return View();
         }
+
+
+
 
 
     }
---
+++ Front-end/BookList/BookList/Models/Books.cs
@@ -0,0 +1,22 @@
+﻿namespace BookList.Models
+{
+    public class Books
+    {
+
+        public int idBooks { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string sinopsis { get; set; }
+        public string idiom { get; set; }
+        public int idGenre { get; set; }
+        public int pages { get; set; }
+        public int idAuthor { get; set; }
+    }
+    public class BookViewModel
+    {
+        public List<Books> Books { get; set; }
+
+    }
+
+
+}
